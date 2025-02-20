aaebd7d61cbdc2853d6e91baad964b6956e4264a
david.rzepa <david@parallelnft.com>
2025-02-19 18:14:05 -0600

    feat: PlayableDirector play when tween plays

---
+++ Samples~/Modules.PlayableDirector/Components/TweenPlayableDirector.cs
@@ -0,0 +1,27 @@
+using HyperTween.ECS.Invoke.Components;
+using HyperTween.ECS.Update.Components;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace HyperTween.Modules.PlayableDirector.Components
+{
+    public class TweenPlayableDirector : ITweenInvokeOnPlay
+    {
+        public UnityEngine.Playables.PlayableDirector PlayableDirector;
+        public PlayableAsset PlayableAsset;
+        
+        public void Invoke(in TweenDuration tweenDuration)
+        {
+            var speed = PlayableAsset.duration / tweenDuration.Value;
+
+            PlayableDirector.Play(PlayableAsset);
+            
+            if (!PlayableDirector.playableGraph.IsValid())
+            {
+                PlayableDirector.RebuildGraph();
+            }
+
+            PlayableDirector.playableGraph.GetRootPlayable(0).SetSpeed(speed);
+        }
+    }
+}
